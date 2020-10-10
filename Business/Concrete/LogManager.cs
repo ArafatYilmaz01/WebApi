13965fb5e399c3fc9984e83d145fc30a332fa57a
Arafat Yilmaz <yilmaz.arafat@gmail.com>
2020-10-11 00:32:27 +0300

    V2

---
+++ Business/Concrete/LogManager.cs
@@ -0,0 +1,38 @@
+﻿using Business.Abstract;
+using Core.DataAccess;
+using Entities;
+using System;
+using System.Collections.Generic;
+using WebApi.DataAccess;
+
+namespace Business.Concrete
+{
+    public class LogManager : ILogService
+
+    {
+        private LogRepository<LogContext> _logRepository= new LogRepository<LogContext>();
+        
+        
+
+        public void Add(LogContext log)
+        {
+            _logRepository.Add(log);
+        }
+
+        public void Delete(int log)
+        {
+           
+            _logRepository.Delete(log);
+        }
+
+        public List<LogContext> GetAll()
+        {
+            return (List<LogContext>)_logRepository.GetAll();
+        }
+
+        public void Save(LogContext log)
+        {
+            _logRepository.Save();
+        }
+    }
+}
